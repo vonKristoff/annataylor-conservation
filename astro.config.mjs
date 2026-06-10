81756c5ef5538cb5097d64610a8bde06fff55685
JCEEE <0xjceee@proton.me>
2026-06-10 15:21:49 +0100

    init repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://annataylorconservation.com",
+  integrations: [sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        $lib: "/src",
+        $components: "/src/components",
+      },
+    },
+  },
+});
