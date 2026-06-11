ecdbc5874a36c88482ac82bb9c86198be3808c13
JCEEE <0xjceee@proton.me>
2026-06-11 16:38:14 +0100

    create manifest

---
+++ src/manifest.js
@@ -0,0 +1,24 @@
+// Auto-generated manifest file
+const fileManifest = [
+  "02.29.webp",
+  "11.33.webp",
+  "21.38.webp",
+  "22.06.webp",
+  "23.03.webp",
+  "Acutty.webp",
+  "Drovers.webp",
+  "Figureheads+1.webp",
+  "Franz+West+Heads.webp",
+  "Helmet2.webp",
+  "IMG_3676+2.webp",
+  "IMG_4409.webp",
+  "IMG_6365.webp",
+  "Mougins+Sagophagus.webp",
+  "P1020771.webp",
+  "P1050681.webp",
+  "Queens+stairs+1.webp",
+  "St+Pauls.webp",
+  "Watts+Gallery+horse.webp"
+];
+
+export default fileManifest;
